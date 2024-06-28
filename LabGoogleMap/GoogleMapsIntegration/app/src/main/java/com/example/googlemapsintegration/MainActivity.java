58a39913c96712570b6bdf793cb1f6ab828fb998
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-06-28 20:37:17 +0700

    update lab google map

---
+++ LabGoogleMap/GoogleMapsIntegration/app/src/main/java/com/example/googlemapsintegration/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.googlemapsintegration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpenMap = findViewById(R.id.btnOpenMap);
+
+        btnOpenMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
